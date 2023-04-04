71cb8b5bea46875cbb9f2bc5e8ac5eeb028da859
Vitor de Paula <vitordcode@gmail.com>
2023-04-03 21:14:52 -0300

    tailwind configurado

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
+      <main className='bg-blue-700 min-h-screen'>
+        <h1>Ola mundo</h1>
       </main>
     </>
   )
