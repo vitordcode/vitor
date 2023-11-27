f69c1d991e8dc352516493ff91110cf914106e1f
Vitor de Paula <vitordepaula98@hotmail.com>
2023-11-27 18:11:33 -0300

    melhorando header e hero home

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,15 +1,39 @@
 import Link from 'next/link'
 import { Button } from '../components/ui/button'
+import { Card } from '../components/ui/card'
+
+import Nefropapers from '../../../public/images/nefropapers.png'
+import Empreart from '../../../public/images/empreart.png'
+
+interface ProjectsProps {
+  id: number,
+  name: string
+  image: string
+}
+
+const Projects = [
+  {
+    id: 1,
+    name: Nefropapers,
+    image: Nefropapers
+  },
+  {
+    id: 2,
+    name: Empreart,
+    image: Empreart,
+  }
+]
 
 export default function Home() {
   return (
     <div className="w-full h-screen px-6 md:px-0 mx-auto">
-      <div className="h-full flex flex-col items-start justify-center">
-        <h1 className="font-semibold text-6xl tracking-tighter">
-          Olá, eu sou Vitor
+      <div className="h-full flex flex-col items-start justify-center max-w-6xl mx-auto">
+        <h1 className="font-semibold text-4xl lg:text-6xl tracking-tighter">
+          Olá, eu sou Vitor<span className="text-primary">.</span> 
+          <br/>Front-end Developer
         </h1>
-        <p className="text-muted-foreground py-4">
-          unindo design e codigo, sigo criando projetos unicos.
+        <p className="text-muted-foreground py-4 font-inter font-regular text-base lg:text-lg lg:max-w-[50%]">
+          Desenvolvendo soluções digitais únicas e eficientes, sempre com o objetivo de transformar ideias em realidade.
         </p>
         <div className='flex items-center gap-2 pt-4'>
           <Button>Meus projetos</Button>
@@ -17,6 +41,14 @@ export default function Home() {
           <Button variant={'link'}>Entrar em contato</Button>
         </div>
       </div>
+
+      <section className='max-w-6xl mx-auto'>
+        <h2 className='font-grotesk font-semibold text-2xl lg:text-3xl'>Projetos mais recentes</h2>
+
+        <div className='grid grid-cols-1 lg:grid-cols-3'>
+          
+        </div>
+      </section>
     </div>
   )
 }
--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -1,56 +1,59 @@
+import Link from 'next/link'
 
-import Link from "next/link"
-
-
-import { Button } from "../ui/button"
-import { HomeIcon, ListOrderedIcon, MenuIcon, PercentIcon } from "lucide-react"
-import { Sheet, SheetClose, SheetContent, SheetTrigger } from "../ui/sheet"
-import { Card } from "../ui/card"
-
+import { Button } from '../ui/button'
+import { HomeIcon, ListOrderedIcon, MenuIcon, PercentIcon } from 'lucide-react'
+import { Sheet, SheetClose, SheetContent, SheetTrigger } from '../ui/sheet'
+import { Card } from '../ui/card'
 
 const Header = () => {
   return (
-    <header className="max-w-6xl mx-auto fixed top-0 w-full">
-      <Card className="w-full flex items-center justify-between py-6 px-6 md:px-0">
-      <Link href={'/'} className="font-bold">
-        vitordcode.
-      </Link>
+    <header className="w-full fixed top-0">
+      <Card className="w-full ">
+        <div className="max-w-6xl mx-auto flex items-center justify-between py-6 px-6 md:px-0">
+          <Link href={'/'} className="font-medium font-grotesk text-lg">
+            vitordcode<span className="text-primary">.</span>
+          </Link>
 
-      <Sheet>
-        <SheetTrigger asChild>
-          <Button size="icon" variant="outline">
-            <MenuIcon />
-          </Button>
-        </SheetTrigger>
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button size="icon" variant="outline">
+                <MenuIcon />
+              </Button>
+            </SheetTrigger>
 
-        <SheetContent side="right">
-
-          <div className="mt-4 flex flex-col gap-3">
-
-            <Button variant="outline" className="w-full justify-start gap-1">
-              <HomeIcon size={16} />
-              Inicio
-            </Button>
-
-            <Button variant="outline" className="w-full justify-start gap-1">
-              <PercentIcon size={16} />
-              Ofertas
-            </Button>
-
-            <SheetClose asChild>
-              <Link href={`/catalog`}>
+            <SheetContent side="right">
+              <div className="mt-4 flex flex-col gap-3">
                 <Button
                   variant="outline"
                   className="w-full justify-start gap-1"
                 >
-                  <ListOrderedIcon size={16} />
-                  Catálogo
+                  <HomeIcon size={16} />
+                  Inicio
                 </Button>
-              </Link>
-            </SheetClose>
-          </div>
-        </SheetContent>
-      </Sheet>
+
+                <Button
+                  variant="outline"
+                  className="w-full justify-start gap-1"
+                >
+                  <PercentIcon size={16} />
+                  Ofertas
+                </Button>
+
+                <SheetClose asChild>
+                  <Link href={`/catalog`}>
+                    <Button
+                      variant="outline"
+                      className="w-full justify-start gap-1"
+                    >
+                      <ListOrderedIcon size={16} />
+                      Catálogo
+                    </Button>
+                  </Link>
+                </SheetClose>
+              </div>
+            </SheetContent>
+          </Sheet>
+        </div>
       </Card>
     </header>
   )
